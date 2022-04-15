bd0803998e961371af79628094ddc56c96736645
Дмитрий Ундозеров <undozerovda@consultant.ru>
2022-04-15 14:19:45 +0300

    Common. Optimized Dockerfile. Added production compose script

---
+++ Compose.bat
@@ -0,0 +1,6 @@
+
+::docker build -f "D:\Projects\boards\Board.Web\Dockerfile" -t boardweb:dev --target final "D:\Projects\boards"
+::docker run -p 8080:80 -e ASPNETCORE_ENVIRONMENT=Docker -v D:\Boards:/usr/share/boards --name Boards boardweb:dev
+docker-compose  -f "docker-compose.yml" -f "docker-compose.override.yml"  -p boards --ansi never up -d --build --remove-orphans board.web
+
+pause
