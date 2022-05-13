a156181f531a85941b80023314b67ec90a63a410
Дмитрий Ундозеров <undozerovda@consultant.ru>
2022-05-13 12:56:11 +0300

    Common. Migrated to .NET 6.0

--- Board.Web/Middlewares/ValidationBehavior.cs
+++ Board.Web/Middlewares/ValidationBehavior.cs
@@ -11,7 +11,7 @@ using FluentValidation.Results;
 using MediatR;
 
 namespace Board.Web.Middlewares {
-	public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> {
+	public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse> {
 		private readonly IEnumerable<IValidator<TRequest>> _validators;
 
 		public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => _validators = validators ?? new List<IValidator<TRequest>>();
