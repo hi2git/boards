a2d9b8ec593836d1fdb379b3047c244521614fe7
undozerovda <undozerovda@consultant.ru>
2022-11-14 16:16:28 +0400

    Users. Adds domain tests

---
+++ Boards.Users.Domain.Tests/UserData.cs
@@ -0,0 +1,31 @@
+﻿using Board.Domain.Enums;
+
+using Boards.Users.Domain.Models;
+
+namespace Boards.Users.Domain.Tests {
+	internal static class UserData {
+
+		public static User Create() => new(ID, NAME, PASSWORD, ROLE, EMAIL);
+
+		public static IEnumerable<object[]> Props {
+			get {
+				yield return new object[] { nameof(User.Id), ID };
+				yield return new object[] { nameof(User.Name), NAME };
+				yield return new object[] { nameof(User.Password), PASSWORD };
+				yield return new object[] { nameof(User.RoleId), ROLE };
+				yield return new object[] { nameof(User.Email), EMAIL };
+			}
+		}
+
+		#region Private
+
+		private static readonly Guid ID = Guid.NewGuid();
+		private static readonly string NAME = "name01";
+		private static readonly string PASSWORD = "password01";
+		private static readonly RoleEnum ROLE = RoleEnum.Admin;
+		private static readonly string EMAIL = "email01";
+
+		#endregion
+
+	}
+}
---
+++ Boards.Users.Domain.Tests/UserTests.cs
@@ -0,0 +1,17 @@
+﻿using Boards.Users.Domain.Models;
+
+
+using NUnit.Framework;
+
+namespace Boards.Users.Domain.Tests {
+	public class UserTests {
+
+		[Test, TestCaseSource(typeof(UserData), nameof(UserData.Props))]
+		public void CtorTest(string propName, object expected) {
+			var entity = UserData.Create();
+			var actual = entity.GetType().GetProperty(propName).GetValue(entity);
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+	}
+}
